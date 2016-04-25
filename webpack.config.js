af8950e73db9c875304402f1731a2a7faeb75d1d
Dan Levy <dan@danlevy.net>
2016-04-24 21:02:18 -0600

    playing with the react-hot-loader v3 latest updates

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3003',
+    'webpack/hot/only-dev-server',
+    './app/index'
+  ],
+  output: {
+    path: path.join(__dirname, '.tmp/public/js'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app')
+    }]
+  }
+};
