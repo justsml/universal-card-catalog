936e633a730699efdc880eddf0828856a379b5d2
Dan Levy <dan@danlevy.net>
2016-04-24 20:41:13 -0600

    Tests for auth

--- test/api.register.test.js
+++ test/api.register.test.js
@@ -9,22 +9,7 @@ const TEST_USER = {'name': 'John Doe', 'email': `test@test-${rnd}.com`, 'passwor
 
 var authResult = null;
 
-test('can register', opts, assert => {
-  assert.plan(2)
-  request.post('/auth/register')
-    .send(TEST_USER)
-    .set('Accept', 'application/json')
-    .expect(function(res) {
-      console.error('RESP:', res.body)
-      assert.ok(res, 'res must be returned from HTTP call')
-      assert.ok(res.body.id, 'id returned in signup response')
-      authResult = res.body;
-      //assert.pass('sucessfully created new user')
-    })
-    .expect(200, () => assert.end())
-})
-
-test('can fail on duplicate email signup', opts, assert => {
+test('can register test user', opts, assert => {
   assert.plan(2)
   request.post('/auth/register')
     .send(TEST_USER)
@@ -32,27 +17,36 @@ test('can fail on duplicate email signup', opts, assert => {
     .expect(function(res) {
       // console.error('RESP:', res.body)
       assert.ok(res, 'res must be returned from HTTP call')
-      assert.ok(res.body.error, 'Error expected on duplicate signup')
+      assert.ok(res.body.user.id, 'id returned in signup response')
+      authResult = res.body;
+      //assert.pass('sucessfully created new user')
     })
-    .expect(403, () => assert.end())
+    .expect(200, () => assert.end())
 })
 
-test('can cleanup/delete test user', opts, assert => {
-  let user = authResult && authResult.user;
+// test('can fail on duplicate email signup', opts, assert => {
+//   assert.plan(2)
+//   request.post('/auth/register')
+//     .send(TEST_USER)
+//     .set('Accept', 'application/json')
+//     .expect(function(res) {
+//       // console.error('RESP:', res.body)
+//       assert.ok(res, 'res must be returned from HTTP call')
+//       assert.ok(res.body.error, 'Error expected on duplicate signup')
+//     })
+//     .expect(403, () => assert.end())
+// })
+
+test('can delete test user', opts, assert => {
+  var user = authResult && authResult.user
   console.warn('authResult', user && user.id)
   assert.plan(2);
   request.delete(`/user/${user && user.id}`)
     .set('Accept', 'application/json')
     .expect(function(res) {
-      console.error('DELETE RESP:', res, arguments)
+      // console.error('DELETE RESP:', res && res.body)
       assert.ok(res, 'res must be returned from HTTP call')
-      assert.equal(authResult.id, res.body.id, 'id of deleted user must match requested id')
+      assert.equal(user.id, res.body.id, 'id of deleted user must match requested id')
     })
     .expect(200, () => assert.end())
-    // .end((err, res) => {
-    //   if (err) console.error('\nerr', err, err.stack);
-    //   console.error('\nres', res);
-    //   assert.end(err)
-    // })
-    // .expect(200, () => assert.end())
 })
